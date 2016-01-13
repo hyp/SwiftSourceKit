125e520ecd65566f07828808e2f1b3735557ca9a
Alex Lorenz <arphaman@gmail.com>
2016-01-13 13:19:53 +0000

    Make editor request creation methods public

--- SwiftSourceKit/EditorRequest.swift
+++ SwiftSourceKit/EditorRequest.swift
@@ -6,7 +6,7 @@
 import sourcekitd
 
 extension Request {
-    static func createEditorOpenRequest(filename: String, sourceText: String, enableSyntaxMap: Bool = true, enableSubStructure: Bool = false, compilerArgs: [String] = []) -> Request {
+    public static func createEditorOpenRequest(filename: String, sourceText: String, enableSyntaxMap: Bool = true, enableSubStructure: Bool = false, compilerArgs: [String] = []) -> Request {
         return Request(dictionary: [
             KeyRequest: .UID(RequestEditorOpen),
             KeyName: .Str(filename),
@@ -16,7 +16,7 @@ extension Request {
             ], compilerArgs: compilerArgs)
     }
 
-    static func createEditorCloseRequest(filename: String) -> Request {
+    public static func createEditorCloseRequest(filename: String) -> Request {
         return Request(dictionary: [
             KeyRequest: .UID(RequestEditorClose),
             KeyName: .Str(filename),
