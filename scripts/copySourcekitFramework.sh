a1d8fd6c04217e190b60bc8594858f4b66ce4717
Alex Lorenz <arphaman@gmail.com>
2016-04-22 09:48:09 +0100

    Fix script indentation for consistency

--- scripts/copySourcekitFramework.sh
+++ scripts/copySourcekitFramework.sh
@@ -6,7 +6,7 @@ sync
 
 rm -rf ${path}/sourcekitd.framework
 if [ "Release" = "${CONFIGURATION}" ]; then
-	cp -a ${SRCROOT}/../build/Ninja-ReleaseAssert/swift-macosx-x86_64/lib/sourcekitd.framework ${path}
+    cp -a ${SRCROOT}/../build/Ninja-ReleaseAssert/swift-macosx-x86_64/lib/sourcekitd.framework ${path}
 else
-	cp -a ${SRCROOT}/../build/Ninja-DebugAssert/swift-macosx-x86_64/lib/sourcekitd.framework ${path}
+    cp -a ${SRCROOT}/../build/Ninja-DebugAssert/swift-macosx-x86_64/lib/sourcekitd.framework ${path}
 fi
